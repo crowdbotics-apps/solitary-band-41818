7e19c1c5bb80aef9093a74ffc43b095927378a22
Crowdbotics <team@crowdbotics.com>
2023-06-29 19:44:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-band-41818",
+  url: "https://solitary-band-41818.botics.co",
+  api: "https://solitary-band-41818.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
